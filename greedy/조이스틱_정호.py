43d3d3cfa9e933a5363b6d6cf8849ed891e7353d
kimjh7669 <55736215+kimjh7669@users.noreply.github.com>
2022-06-05 16:45:15 +0900

    Update 조이스틱_정호.py

--- greedy/조이스틱_정호.py
+++ greedy/조이스틱_정호.py
@@ -1,37 +1,18 @@
 def solution(name):
     answer = 0
     to_list = []
-
-    for idx, char in enumerate(name):
-        num = ord(char)
-        if num == 65:
-            continue        
-        if num > 77:
-            answer += (91-num)
-            to_list.append(idx)
-        else:
-            answer += (num-65)
-            to_list.append(idx)
-    if len(to_list)== 0:
-        min_num = 0    
-    elif len(to_list) == 1:
-        min_num = to_list[0]
-        if min_num > len(name) - to_list[0]:
-            min_num = len(name) - to_list[0]
-    else:
-        min_num = len(name)-to_list[0]
+    min_num = len(name) -1
+    for idx, s in enumerate(name):
+        if ord(s) - 65 < 13:    answer += ord(s) - 65
+        else:   answer += 25 - (ord(s) - 66)    
+        next_idx = idx+1
+        while next_idx < len(name) and name[next_idx] == 'A':
+            next_idx += 1
+        min_num = min(min_num, 2*idx + len(name) - next_idx)
+        min_num = min(min_num, idx + 2*(len(name)-next_idx))
         
-        for i in range(len(to_list)):
-            if i == len(to_list)-1:
-                if min_num > to_list[i]:
-                    min_num = to_list[i]
-            else:
-                if min_num > (2*to_list[i]+(len(name) - to_list[i+1])):
-                    min_num = (2*to_list[i]+(len(name) - to_list[i+1]))
-                if min_num > (to_list[i]+2*(len(name) - to_list[i+1])):
-                    min_num = (to_list[i]+2*(len(name) - to_list[i+1]))
-            
-
     answer += min_num
 
     return answer
+        
+      
