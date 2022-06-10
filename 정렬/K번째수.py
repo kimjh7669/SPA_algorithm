d2bc567e57224eea842bbb1ef0ee15c8f264fbf9
kimjh7669 <55736215+kimjh7669@users.noreply.github.com>
2022-06-11 02:23:49 +0900

    Create K번째수.py

---
+++ 정렬/K번째수.py
@@ -0,0 +1,7 @@
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        tmp = array[command[0]-1:command[1]]
+        tmp = sorted(tmp)
+        answer.append(tmp[command[2]-1])
+    return answer
